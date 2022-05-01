2e732fb2512eb23b98b795c6a901488ceaa1d0f5
PhilosopheAM <11911421@mail.sustech.edu.cn>
2022-05-01 18:56:57 +0800

    修改杜良代码连接到数据库

--- FDL_Front/html/connect_to_db.php
+++ FDL_Front/html/connect_to_db.php
@@ -19,14 +19,14 @@
 
 
     // SQL sentence to excute the command
-    $sql = "SELECT * FROM user WHERE id = 1";
+    // $sql = "SELECT * FROM user WHERE id = 1";
 
     // If you have cookies reserved, use the command like this:
     // $XXX = XXX
     // $sql = "SELECT * FROM user WHERE XXX ='$XXX'";
 
-    $query_result_information = mysqli_query($connection,$sql);
+    // $query_result_information = mysqli_query($connection,$sql);
     
     // We use assoc style here. You can acquire the return information through both numeric id or string key.
-    $information = mysqli_fetch_assoc($query_result_information);
+    // $information = mysqli_fetch_assoc($query_result_information);
 ?>
---
+++ FDL_Front/html/connect_to_db_article.php
@@ -0,0 +1,13 @@
+<?php
+    // SQL sentence to excute the command
+    $sql = "SELECT * FROM user_like_article WHERE article_id = 1";
+
+    // If you have cookies reserved, use the command like this:
+    // $XXX = XXX
+    // $sql = "SELECT * FROM user WHERE XXX ='$XXX'";
+
+    $query_result_information = mysqli_query($connection,$sql);
+    
+    // We use assoc style here. You can acquire the return information through both numeric id or string key.
+    $information = mysqli_fetch_assoc($query_result_information);
+?>
